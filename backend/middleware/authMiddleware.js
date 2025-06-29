9231a3a051a5f47467c37fe028fca613e5210ed3
yuvraj-sapkota <sharmayuvraj034@gmail.com>
2025-06-30 00:09:10 +0545

    changed

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -2,6 +2,7 @@ const jwt = require("jsonwebtoken");
 
 const authMiddleware = (req, res, next) => {
   const authHeader = req.headers.authorization;
+  
 
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
     return res.status(401).json({ message: "No token provided" });
@@ -12,7 +13,7 @@ const authMiddleware = (req, res, next) => {
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
-    console.log(req.user);
+    
     next();
   } catch (err) {
     return res.status(401).json({ message: "Invalid or expired token" });
