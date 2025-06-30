36f414655baa25d0b6569bf825f808a0cc6d6be6
yuvraj-sapkota <sharmayuvraj034@gmail.com>
2025-06-30 08:17:16 +0545

    changes done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -21,7 +21,7 @@ const App = () => {
           <Route path="/register" element={<SignupPage />} />
         </Route>
 
-        <Route path="/menupage?restaurantId" element={<MenuPage />} />
+        <Route path="/menupage" element={<MenuPage />} />
 
         {/* protected route  */}
         <Route element={<ProtectedRoute />}>
