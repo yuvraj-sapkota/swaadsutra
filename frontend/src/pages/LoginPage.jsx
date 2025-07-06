7cf776d615a2e59a4ab385d86f883295f3205f5b
yuvraj-sapkota <sharmayuvraj034@gmail.com>
2025-07-06 18:17:29 +0545

    upload changes

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -90,8 +90,8 @@ const LoginPage = () => {
               </button>
             </div>
 
-            <div>
-              <p className="text-[#2E8B57]">Forgot Password?</p>
+            <div className="-mt-6">
+              <p className="text-[#2E8B57] text-end">Forgot Password?</p>
             </div>
 
             <Button type="submit">Login</Button>
